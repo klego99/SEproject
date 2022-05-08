d9edd0067e980748a3adb4a977cee5e66a45d270
KimJongHeon <klego98@gmail.com>
2022-05-08 21:46:27 +0900

    timer not ready

--- app/src/main/java/com/gachon/myapplication/Schedule2Activity.java
+++ app/src/main/java/com/gachon/myapplication/Schedule2Activity.java
@@ -5,87 +5,37 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.os.SystemClock;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import org.w3c.dom.Text;
-
-import java.sql.Time;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
-import java.util.Timer;
-import java.util.TimerTask;
 
-public class ScheduleActivity extends AppCompatActivity {
+public class Schedule2Activity extends AppCompatActivity {
 
     // Changes
+    private long baseTime,pauseTime;
+    public static final int INIT=0;
+    public static final int RUN=1;
+    public static final int PAUSE=2;
+    public static int status=INIT;
     private TextView clock;
     private Button startBtn;
-    private Button resetBtn;
-    TextView hourT;
-    TextView minuteT;
-    TextView secondT;
-    TextView finishment;
-    EditText hourE;
-    EditText minuteE;
-    EditText secondE;
-    LinearLayout linearLayoutTV;
-    LinearLayout linearLayoutET;
-    int hour,minute, second;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.acticity_schdule);
+        clock = findViewById(R.id.Clock);
         startBtn = findViewById(R.id.start_btn);
-        resetBtn = findViewById(R.id.reset_btn);
-        hourT=findViewById(R.id.hour);
-        minuteT=findViewById(R.id.minute);
-        secondT=findViewById(R.id.second);
-        hourE=findViewById(R.id.hourE);
-        minuteE=findViewById(R.id.minuteE);
-        secondE=findViewById(R.id.secondE);
-        linearLayoutTV=findViewById(R.id.timerTV);
-        linearLayoutET=findViewById(R.id.timerET);
-        finishment=findViewById(R.id.finish);
-        startBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                linearLayoutET.setVisibility(View.GONE);
-                linearLayoutTV.setVisibility(View.VISIBLE);
-
-               hourT.setText(hourE.getText().toString());
-               minuteT.setText(minuteE.getText().toString());
-               secondT.setText(secondE.getText().toString());
-
-               hour=Integer.parseInt(hourE.getText().toString());
-               minute=Integer.parseInt(minuteE.getText().toString());
-               second=Integer.parseInt(secondE.getText().toString());
-               Log.i("time",hour+"/"+minute+"/"+second);
-                Timer timer= new Timer();
-                TimerTask timerTask= new TimerTask() {
-                    @Override
-                    public void run() {
-                        timerSetting(timer);
-                    }
-                };
-                timer.schedule(timerTask,0,1000);
-            }
-
-        });
-        resetBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-            }
-        });
+        baseTime= SystemClock.elapsedRealtime();//time check
+        startBtn.setOnClickListener(onClickListener);
         //image button
         ImageButton Block = findViewById(R.id.Block);
         Block.setOnClickListener(new View.OnClickListener() {
@@ -143,49 +93,43 @@ public class ScheduleActivity extends AppCompatActivity {
             }
         });
     }
-    public void timerSetting(Timer timer){
-        if(second!=0){
-            second--;
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            staButton();
         }
-        else if(minute !=0){
-            second=60;
-            second--;
-            minute--;
-        }else if(hour !=0){
-            second= 60;
-            minute=60;
-            second--;
-            minute--;
-            hour--;
-        }
+    };
+    public void staButton(){
+        switch (status){
+            case INIT:
+                baseTime =SystemClock.elapsedRealtime();
+                handler.sendEmptyMessage(0);
+                pauseTime=SystemClock.elapsedRealtime();
+                startBtn.setText("멈춤2");
+                status=RUN;
+                break;
+            case RUN:
+                handler.removeMessages(0);
+                startBtn.setText("시작");
+                pauseTime = SystemClock.elapsedRealtime();
+                clock.setText("00:00:00");
+                status=INIT;
+                break;
 
-        if(second<=9){
-            secondT.setText("0"+second);
-        }else{
-            secondT.setText(Integer.toString(second));
         }
-
-        if(minute<=9){
-            minuteT.setText("0"+minute);
-        }else{
-            minuteT.setText(Integer.toString(minute));
-        }
-
-        if(hour<=9){
-            hourT.setText("0"+hour);
-        }else{
-            hourT.setText(Integer.toString(hour));
-        }
-
-        if(hour==0&&minute==0&&second==0){
-            timer.cancel();
-            finishment.setText("종료되었습니다.");
-        }
+    }
+    public String getTime(){
+        //time check
+        long nowtime=SystemClock.elapsedRealtime();
+        long overtime= nowtime-baseTime;
+        SimpleDateFormat format= new SimpleDateFormat("HH:mm:ss", Locale.getDefault());
+        String recTime =format.format(overtime);
+        return recTime;
     }
     Handler handler = new Handler(){
         @Override
         public void handleMessage(@NonNull Message msg){
-            hourT.setText("0"+hour);
+            clock.setText(getTime());
             handler.sendEmptyMessage(0);
         }
     };
